35faf70a235bcb66016d0e699e1c103f3126918f
Sgiath <sgiath@sgiath.dev>
2022-06-24 15:48:34 +0200

    feat: better prod config
    Signed-off-by: Sgiath <sgiath@sgiath.dev>

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :web, Web.Endpoint,
-  url: [host: "sgiath.dev", port: 443],
+  url: [scheme: "https", host: "sgiath.dev", port: 443],
   force_ssl: [hsts: true],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
--- config/runtime.exs
+++ config/runtime.exs
@@ -11,9 +11,7 @@ if config_env() == :prod do
   config :web, Web.Endpoint,
     server: true,
     http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      ip: {0, 0, 0, 0, 0, 0, 0, 1},
+      ip: {127, 0, 0, 1},
       port: String.to_integer(System.get_env("PORT") || "4000")
     ],
     secret_key_base: secret_key_base
